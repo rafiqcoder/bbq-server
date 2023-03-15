92bd2fcac5c0a174277258fbdf93f8dee1ba1954
rafiqcoder <rafiqcoder@gmail.com>
2023-03-15 17:29:12 +0600

    refresh db

--- middleware/verifyJwt.js
+++ middleware/verifyJwt.js
@@ -2,36 +2,46 @@ const jwt = require('jsonwebtoken');
 module.exports.veryfyJwt = async (req,res,next) => {
     try {
         const { authorization } = req.headers;
-        const { refreshToken } = req.cookies;
+        // const { refreshToken } = req.cookies;
         // console.log(req);
         // console.log(authorization);
         // console.log(authHeader);
         // console.log('rea',req)
-        console.log('refreshToken',refreshToken);
         const token = authorization && authorization.split(' ')[1];
         if (token === null) return res.status(401).json({ error: 'Unauthorized' });
         jwt.verify(token,process.env.ACCESS_SECRET_TOKEN,(err,user) => {
             if (err) {
                 // res.status(403).json({ error: 'unauthorized true' })
-                // const refreshToken = 
-                jwt.verify(refreshToken,process.env.ACCESS_REFRESH_TOKEN,(err,user) => {
-                    if (err) {
-                        return res.status(403).json({
-                            logout: true,
-                            error: err,
-                            message: 'Please login again',
+                const foundUser = await userModels.findOne({ email: user.email });
+                const refreshToken = foundUser.refreshToken;
+                if (refreshToken) {
+                    jwt.verify(refreshToken,process.env.ACCESS_REFRESH_TOKEN,(err,user) => {
+                        if (err) {
+                            return res.status(403).json({
+                                logout: true,
+                                error: err,
+                                message: 'Please login again',
 
-                        });
-                    }
-                    const accessToken = jwt.sign({ email: user.email },process.env.ACCESS_SECRET_TOKEN,{ expiresIn: '15s' });
-
-
-                    req.accessToken = accessToken;
-                    req.user = user;
-                    next();
+                            });
+                        }
+                        const accessToken = jwt.sign({ email: user.email },process.env.ACCESS_SECRET_TOKEN,{ expiresIn: '15s' });
 
 
-                })
+                        req.accessToken = accessToken;
+                        req.user = user;
+                        next();
+
+
+
+                    })
+                } else {
+                    return res.status(403).json({
+                        logout: true,
+                        error: err,
+                        message: 'Please login again',
+
+                    });
+                }
 
             } else {
                 req.user = user;
