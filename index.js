b32143db157d2b7a2077e727d01a526a1e81d19e
rafiqcoder <rafiqcoder@gmail.com>
2023-03-15 15:54:10 +0600

    cors

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
-app.use(cors(
-    {
-        origin: 'https://bbq.netlify.app',
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 const SSLCommerzPayment = require('sslcommerz-lts')
